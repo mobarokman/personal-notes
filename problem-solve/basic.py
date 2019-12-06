43b4bfad5c67fb89945aa64cd468b339c79b7f59
mobarokman <mobarokman@gmail.com>
2019-12-06 22:34:59 +0600

    make note

--- problem-solve/basic.py
+++ problem-solve/basic.py
@@ -1,16 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+integerVal = input("enter integer")
+doubleVal = input("enter double")
+stringVal = input("enter string")
+# Read and save an integer, double, and String to your variables.
+intSum = int(i) + int(integerVal)
+douSum = float(d) + float(doubleVal)
+strCon = s + stringVal
+# Print the sum of both integer variables on a new line.
+print(intSum)
+# Print the sum of the double variables on a new line.
+print(douSum)
+# Concatenate and print the String variables on a new line
+print(strCon)
+# The 's' variable above should be printed first.
 
-# Python 3 code for printing 
-# on the same line printing  
-# geeks and geeksforgeeks  
-# in the same line 
-  
-print("geeks", end=" ") 
-print("geeksforgeeks", end=' ') 
-  
-# array 
-a = [1, 2, 3, 4] 
-  
-# printing a element in same 
-# line 
-for i in range(4): 
-    print(a[i], end =" ")  
+
